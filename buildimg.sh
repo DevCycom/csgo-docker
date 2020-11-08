ce5f4e59a9e5de822630da9411ed153df4b9a91e
Renard <ludovic.fernandez@epitech.eu>
2020-11-08 21:53:39 +0100

    [FIX] Adapt buildimg.sh to layer compression changes

--- buildimg.sh
+++ buildimg.sh
@@ -2,8 +2,8 @@
 #
 # Automate naming the image for a SteamCMD Docker server
 
-GAME=`grep 'ENV USER ' Dockerfile | perl -pe 's/(?:ENV USER )(\w*)/$1/'`
-VERSION=`grep 'LABEL version=' Dockerfile | perl -pe 's/(?:LABEL version=")(.*)"/$1/'`
+GAME=`grep 'ENV USER=' Dockerfile | perl -pe 's/(?:ENV USER=)(\w*).*$/$1/'`
+VERSION=`grep 'LABEL version=' Dockerfile | perl -pe 's/(?:LABEL version=")((\d|-)+)".*$/$1/'`
 IMG=cycom/${GAME}
 
 docker build --rm -t ${IMG}:${VERSION} -t ${IMG}:latest .
